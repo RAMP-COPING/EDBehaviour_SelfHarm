7296f6e0a7f9ce10f2310176682b6c0790bea20c
Helena-D <helena.davies@kcl.ac.uk>
2021-01-18 15:16:47 +0000

    added libraries as separate source script

---
+++ libraries.R
@@ -0,0 +1,71 @@
+###Libraries
+
+if(!require(tidyverse)){
+  install.packages("tidyverse")
+  library(tidyverse)
+}
+
+if(!require(knitr)){
+  install.packages("knitr")
+  library(knitr)
+}
+
+if(!require(car)){
+  install.packages("car")
+  library(car)
+}
+
+if(!require(psych)){
+  install.packages("psych")
+  library(psych)
+}
+
+if(!require(lubridate)){
+  install.packages("lubridate")
+  library(lubridate)
+}
+
+if(!require(broom)){
+  install.packages("broom")
+  library(broom)
+}
+
+if(!require(ggfortify)){
+  install.packages("ggfortify")
+  library(ggfortify)
+}
+
+if(!require(summarytools)){
+  install.packages("summarytools")
+  library(summarytools)
+}
+
+if(!require(ggformula)){
+  install.packages("ggformula")
+  library(ggformula)
+}
+
+if(!require(data.table)){
+  install.packages("data.table")
+  library(data.table)
+}
+
+if(!require(ggrepel)){
+  install.packages("ggrepel")
+  library(ggrepel)
+}
+
+if(!require(ggpubr)){
+  install.packages("ggpubr")
+  library(ggpubr)
+}
+
+if(!require(UpSetR)){
+  install.packages("UpSetR")
+  library(UpSetR)
+}
+
+if(!require(skimr)){
+  install.packages("skimr")
+  library(skimr)
+}
