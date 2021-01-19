75900be12fa66e5d873fe3681b9caaab0918fa2c
Helena-D <helena.davies@kcl.ac.uk>
2021-01-19 12:30:27 +0000

    COPING waves info + new source script added

---
+++ wavesCOPING.R
@@ -0,0 +1,20 @@
+## Create wave column in COPING
+
+##FORTNIGHTLY:
+#Wave 1, follow up A = 19th May - 26th May
+#Wave 2, follow up B = 2nd June - 9th June (?)
+#Wave 3, follow up A = 16th June - 23 June at 5PM (Set A_ongoing) or Friday, 26 June (setA_1)
+#Wave 4, follow up B = 30th June - 7th July
+#Wave 5, follow up A = 14th July - 21st July
+#Wave 6, follow up B = 28th July - 4th August
+
+##MONTHLY
+#Wave 7, follow up A = 25th August- 15th Sept
+#Wave 8, follow up B = 22nd Sept - 13th Oct
+#Wave 9, follow up A = 20th Oct - 10th Nov
+#Wave 10, follow up B, = 17th Nov - 8th Dec
+#WAVE 11, follow up A = 15th Dec - 5th Jan
+#(Note that waves continue but data extracted in Dec)
+
+#**EDEQ only in follow up B, TAF in both A and B
+
